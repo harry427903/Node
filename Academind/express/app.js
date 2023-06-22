126e05ca29b4c95525fb22f0c8048d9aca23e0de
Anurag-Yadav03 <03yadavanurag@gmail.com>
2023-06-22 17:39:44 +0530

    Completed Second Module

--- Academind/express/app.js
+++ Academind/express/app.js
@@ -2,4 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use('/', (req, res, next) => {
+  console.log('This always runs');
+  next();
+});
+
+app.use('/about-product', (req, res, next) => {
+  console.log("in another parle -G  middleware");
+  res.send('<h1>Hello from about product</h1>');
+});
+
+console.log("Hello from the body");
 app.listen(3000);
